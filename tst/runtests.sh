f59d9b49d74f3c4db5ca2d41f1e1b07389c79311
Milan Toth <milgra@milgra.com>
2022-04-13 14:11:29 +0200

    updates

--- tst/runtests.sh
+++ tst/runtests.sh
@@ -1,7 +1,7 @@
-build/test-tree -c tst/config_a -w tst/workspace_a.json -t tst/tree_a.json -o tst/image_1.bmp
-build/test-tree -c tst/config_b -w tst/workspace_a.json -t tst/tree_a.json -o tst/image_2.bmp
-build/test-tree -c tst/config_a -w tst/workspace_b.json -t tst/tree_b.json -o tst/image_3.bmp
-build/test-tree -c tst/config_b -w tst/workspace_b.json -t tst/tree_b.json -o tst/image_4.bmp
+build/test_tree -c tst/config_a -w tst/workspace_a.json -t tst/tree_a.json -o tst/image_1.bmp
+build/test_tree -c tst/config_b -w tst/workspace_a.json -t tst/tree_a.json -o tst/image_2.bmp
+build/test_tree -c tst/config_a -w tst/workspace_b.json -t tst/tree_b.json -o tst/image_3.bmp
+build/test_tree -c tst/config_b -w tst/workspace_b.json -t tst/tree_b.json -o tst/image_4.bmp
 diff tst/image_1.bmp tst/original_1.bmp
 diff tst/image_2.bmp tst/original_2.bmp
 diff tst/image_3.bmp tst/original_3.bmp
