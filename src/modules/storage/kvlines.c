0f921edbab51f282e57de8c57076857504fb6f91
Milan Toth <milgra@milgra.com>
2022-04-28 14:02:13 +0200

    explicityle including stdio for popen

--- src/modules/storage/kvlines.c
+++ src/modules/storage/kvlines.c
@@ -2,7 +2,6 @@
 #define kvlines_h
 
 #include "zc_map.c"
-#include <stdio.h>
 
 int kvlines_read(char* libpath, map_t* db);
 int kvlines_write(char* libpath, map_t* db);
@@ -13,138 +12,139 @@ int kvlines_write(char* libpath, map_t* db);
 
 #include "zc_cstring.c"
 #include <limits.h>
+#include <stdio.h>
 
 int kvlines_read(char* libpath, map_t* db)
 {
-  int   retv  = -1;
-  char* dbstr = cstr_new_file(libpath); // REL 0
+    int   retv  = -1;
+    char* dbstr = cstr_new_file(libpath); // REL 0
 
-  if (dbstr)
-  {
-    retv = 0;
-
-    char* key = NULL;
-    char* val = NULL;
-
-    int in_word = 0;
-    int in_comm = 0;
-    int newline = 1;
-
-    int word_index = 0;
-    int store_word = 0;
-
-    for (int index = 0; index <= strlen(dbstr); index++)
+    if (dbstr)
     {
-      if (dbstr[index] == '\n' || dbstr[index] == '\0')
-      {
-        // newline or end of file, close word, set new line
-        if (in_word == 1) store_word = 1;
-        in_word = 0;
-        in_comm = 0;
-        newline = 1;
-      }
-      else if (dbstr[index] == ' ' || dbstr[index] == '\t')
-      {
-        if (key == NULL)
-        {
-          // space, close word
-          if (in_word == 1) store_word = 1;
-          in_word = 0;
-        }
-      }
-      else if (dbstr[index] == '#' && newline == 1)
-      {
-        // comment after newline, toggle comment mode
-        in_comm = 1;
-        newline = 0;
-      }
-      else if (in_comm == 0 && in_word == 0)
-      {
-        // if not in comment and not in word, set word mode, store index
-        in_word    = 1;
-        newline    = 0;
-        word_index = index;
-      }
+	retv = 0;
 
-      if (store_word == 1)
-      {
-        store_word = 0;
-        if (key == NULL)
-        {
-          key = cstr_new_cstring("");
-          key = cstr_append_sub(key, dbstr, word_index, index - word_index);
-        }
-        else if (val == NULL)
-        {
-          val = cstr_new_cstring("");
-          val = cstr_append_sub(val, dbstr, word_index, index - word_index);
+	char* key = NULL;
+	char* val = NULL;
 
-          MPUT(db, key, val);
+	int in_word = 0;
+	int in_comm = 0;
+	int newline = 1;
 
-          REL(key);
-          REL(val);
+	int word_index = 0;
+	int store_word = 0;
 
-          key = NULL;
-          val = NULL;
-        }
-      }
+	for (int index = 0; index <= strlen(dbstr); index++)
+	{
+	    if (dbstr[index] == '\n' || dbstr[index] == '\0')
+	    {
+		// newline or end of file, close word, set new line
+		if (in_word == 1) store_word = 1;
+		in_word = 0;
+		in_comm = 0;
+		newline = 1;
+	    }
+	    else if (dbstr[index] == ' ' || dbstr[index] == '\t')
+	    {
+		if (key == NULL)
+		{
+		    // space, close word
+		    if (in_word == 1) store_word = 1;
+		    in_word = 0;
+		}
+	    }
+	    else if (dbstr[index] == '#' && newline == 1)
+	    {
+		// comment after newline, toggle comment mode
+		in_comm = 1;
+		newline = 0;
+	    }
+	    else if (in_comm == 0 && in_word == 0)
+	    {
+		// if not in comment and not in word, set word mode, store index
+		in_word    = 1;
+		newline    = 0;
+		word_index = index;
+	    }
+
+	    if (store_word == 1)
+	    {
+		store_word = 0;
+		if (key == NULL)
+		{
+		    key = cstr_new_cstring("");
+		    key = cstr_append_sub(key, dbstr, word_index, index - word_index);
+		}
+		else if (val == NULL)
+		{
+		    val = cstr_new_cstring("");
+		    val = cstr_append_sub(val, dbstr, word_index, index - word_index);
+
+		    MPUT(db, key, val);
+
+		    REL(key);
+		    REL(val);
+
+		    key = NULL;
+		    val = NULL;
+		}
+	    }
+	}
+
+	REL(dbstr);
     }
 
-    REL(dbstr);
-  }
-
-  return retv;
+    return retv;
 }
 
 int kvlines_write(char* libpath, map_t* db)
 {
-  int   retv = -1;
-  char* path = cstr_new_format(PATH_MAX + NAME_MAX, "%snew", libpath); // REL 0
-  FILE* file = fopen(path, "w");                                       // CLOSE 0
+    int   retv = -1;
+    char* path = cstr_new_format(PATH_MAX + NAME_MAX, "%snew", libpath); // REL 0
+    FILE* file = fopen(path, "w");                                       // CLOSE 0
 
-  if (file)
-  {
-    retv        = 0;
-    vec_t* vals = VNEW(); // REL 1
-    map_values(db, vals);
-
-    for (int vali = 0; vali < vals->length; vali++)
+    if (file)
     {
-      map_t* entry = vals->data[vali];
-      vec_t* keys  = VNEW(); // REL 2
+	retv        = 0;
+	vec_t* vals = VNEW(); // REL 1
+	map_values(db, vals);
 
-      map_keys(entry, keys);
+	for (int vali = 0; vali < vals->length; vali++)
+	{
+	    map_t* entry = vals->data[vali];
+	    vec_t* keys  = VNEW(); // REL 2
 
-      for (int keyi = 0; keyi < keys->length; keyi++)
-      {
-        char* key = keys->data[keyi];
-        char* val = MGET(entry, key);
+	    map_keys(entry, keys);
 
-        if (fprintf(file, "%s %s\n", key, val) < 0) retv = -1;
-      }
+	    for (int keyi = 0; keyi < keys->length; keyi++)
+	    {
+		char* key = keys->data[keyi];
+		char* val = MGET(entry, key);
 
-      REL(keys); // REL 2
+		if (fprintf(file, "%s %s\n", key, val) < 0) retv = -1;
+	    }
 
-      if (retv < 0) break;
-    }
+	    REL(keys); // REL 2
 
-    if (fclose(file) == EOF) retv = -1; // CLOSE 0
+	    if (retv < 0) break;
+	}
 
-    REL(vals); // REL 1
+	if (fclose(file) == EOF) retv = -1; // CLOSE 0
 
-    if (retv == 0)
-    {
-      if (rename(path, libpath) != 0) retv = -1;
+	REL(vals); // REL 1
+
+	if (retv == 0)
+	{
+	    if (rename(path, libpath) != 0) retv = -1;
+	}
+	else
+	    printf("ERROR kvlines_write cannot write file\n");
     }
     else
-      printf("ERROR kvlines_write cannot write file\n");
-  }
-  else
-    printf("ERROR kvlines_write cannot open file %s\n", path);
+	printf("ERROR kvlines_write cannot open file %s\n", path);
 
-  REL(path); // REL 0
+    REL(path); // REL 0
 
-  return retv;
+    return retv;
 }
 
 #endif
--- src/sov/config/fontconfig.c
+++ src/sov/config/fontconfig.c
@@ -9,54 +9,55 @@ char* fontconfig_new_path(char* face_desc);
 
 #include "zc_cstring.c"
 #include "zc_memory.c"
+#include <stdio.h>
 #include <string.h>
 
 char* fontconfig_new_path(char* face_desc)
 {
-  char buff[100];
+    char buff[100];
 
-  char* cstr = cstr_new_cstring(""); // REL 0
+    char* cstr = cstr_new_cstring(""); // REL 0
 
-  /* look for face_desc in fc-list fitting end of row */
-  char* command = cstr_new_format(80, "fc-list | grep '%s'$", face_desc); // REL 1
-  FILE* pipe    = popen(command, "r");                                    // CLOSE 0
-  REL(command);                                                           // REL 1
-
-  while (fgets(buff, sizeof(buff), pipe) != NULL) cstr = cstr_append(cstr, buff);
-  pclose(pipe); // CLOSE 0
-
-  if (strlen(cstr) == 0)
-  {
-    /* if no result, look for face_desc in fc-list inside rows */
-    command    = cstr_new_format(80, "fc-list | grep '%s'", face_desc); // REL 2
-    FILE* pipe = popen(command, "r");                                   // CLOSE 1
-    REL(command);                                                       // REL 2
+    /* look for face_desc in fc-list fitting end of row */
+    char* command = cstr_new_format(80, "fc-list | grep '%s'$", face_desc); // REL 1
+    FILE* pipe    = popen(command, "r");                                    // CLOSE 0
+    REL(command);                                                           // REL 1
 
     while (fgets(buff, sizeof(buff), pipe) != NULL) cstr = cstr_append(cstr, buff);
-    pclose(pipe); // CLOSE 1
+    pclose(pipe); // CLOSE 0
 
     if (strlen(cstr) == 0)
     {
-      /* if no result, get first available font */
-      command    = cstr_new_cstring("fc-list | grep ''$"); // REL 3
-      FILE* pipe = popen(command, "r");                    // CLOSE 2
-      REL(command);                                        // REL  3
+	/* if no result, look for face_desc in fc-list inside rows */
+	command    = cstr_new_format(80, "fc-list | grep '%s'", face_desc); // REL 2
+	FILE* pipe = popen(command, "r");                                   // CLOSE 1
+	REL(command);                                                       // REL 2
 
-      while (fgets(buff, sizeof(buff), pipe) != NULL) cstr = cstr_append(cstr, buff);
-      pclose(pipe); // CLOSE 2
+	while (fgets(buff, sizeof(buff), pipe) != NULL) cstr = cstr_append(cstr, buff);
+	pclose(pipe); // CLOSE 1
+
+	if (strlen(cstr) == 0)
+	{
+	    /* if no result, get first available font */
+	    command    = cstr_new_cstring("fc-list | grep ''$"); // REL 3
+	    FILE* pipe = popen(command, "r");                    // CLOSE 2
+	    REL(command);                                        // REL  3
+
+	    while (fgets(buff, sizeof(buff), pipe) != NULL) cstr = cstr_append(cstr, buff);
+	    pclose(pipe); // CLOSE 2
+	}
     }
-  }
 
-  /* extract file name before double colon */
+    /* extract file name before double colon */
 
-  char* dcolon = strchr(cstr, ':');
-  char* result = cstr_new_cstring("");
+    char* dcolon = strchr(cstr, ':');
+    char* result = cstr_new_cstring("");
 
-  result = cstr_append_sub(result, cstr, 0, dcolon - cstr);
+    result = cstr_append_sub(result, cstr, 0, dcolon - cstr);
 
-  REL(cstr); // REL 0
+    REL(cstr); // REL 0
 
-  return result;
+    return result;
 }
 
 #endif
