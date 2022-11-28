4a76c6c06651e1dd9512d24f167d88c7f1dddaa4
Milan Toth <milgra@milgra.com>
2022-11-28 18:39:30 +0000

    test test

--- tst/runtests.sh
+++ tst/runtests.sh
@@ -30,10 +30,13 @@ diff tst/master tst/result
 error=$?
 if [ $error -eq 0 ]
 then
+    echo "No differences found between master and result images"
     exit 0
 elif [ $error -eq 1 ]
 then
+    echo "Differences found between master and result images"
     exit 1
 else
+    echo "Differences found between master and result images"
     exit 1
 fi
