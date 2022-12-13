b37d8cf054be8a14ce006524fe780e3ab50df9f5
Milan Toth <milgra@milgra.com>
2022-12-13 21:57:13 +0000

    quick fix

--- src/kinetic_ui/ku_connector_wayland.c
+++ src/kinetic_ui/ku_connector_wayland.c
@@ -840,6 +840,9 @@ void ku_wayland_delete_window(wl_window_t* info)
 	wl_surface_destroy(info->surface);
 	wl_display_roundtrip(wlc.display);
     }
+
+    // TODO replace window array with vector
+    wlc.window_count = 0;
 }
 
 void ku_wayland_show_window(wl_window_t* info)
